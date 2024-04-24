2b6060bc6a5bc648a9892591bb5d49f9fba957e6
Kongkon Jowarder <kongkon4545@gmail.com>
2024-04-24 20:09:00 +0600

    axios api use from data fetch

--- lib/getAllPosts.jsx
+++ lib/getAllPosts.jsx
@@ -1,8 +1,21 @@
 
 
+// const getAllPosts = async () => {
+//    const result = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+//    return result.json();
+// };
+
+// export default getAllPosts;
+
+
+import axios from "axios";
 const getAllPosts = async () => {
-   const result = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
-   return result.json();
+  try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10");
+      return response.data;
+  } catch (error) {
+    throw new Error(error)
+  }
 };
 
 export default getAllPosts;
--- lib/getPost.jsx
+++ lib/getPost.jsx
@@ -1,8 +1,20 @@
 
 
+// const getPost = async (id) => {
+//     const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+//    return result.json();
+// };
+
+// export default getPost;
+
+import axios from "axios";
 const getPost = async (id) => {
-    const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-   return result.json();
+    try {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        return response.data;
+    } catch (error) {
+        throw new Error(error)
+    }
 };
 
 export default getPost;
